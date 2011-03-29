e51bbd7607f582a740e7b2613353e55ccaaac271
João Neves <sevenjp@gmail.com>
2011-03-29 14:38:31 +0100

    Generified the default sqlite location in settings, could be empty but meh.

--- hojehatransportes/settings.py
+++ hojehatransportes/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/seven/Documents/blah.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': '%s/blah.sqlite' % os.getcwd(),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
