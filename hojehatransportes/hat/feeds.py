3a34a122c1b9583f365eda1ee951c1207791c2b2
Filipe Cabecinhas <filcab+git@gmail.com>
2011-04-06 00:38:41 +0100

    s/description/comment/

--- hojehatransportes/hat/feeds.py
+++ hojehatransportes/hat/feeds.py
@@ -64,7 +64,7 @@ class IcsFeed(Events):
     def item_end(self, strike):
         return strike.end_date
 
-    def item_description(self, strike):
+    def item_comment(self, strike):
         return 'Greve da empresa ' + strike.company.name + '\n' + 'De ' + str(strike.start_date) + ' a ' + str(strike.end_date.strftime) + '\n' + strike.description
 
     def item_link(self, strike):
