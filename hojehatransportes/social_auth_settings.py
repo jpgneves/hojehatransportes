0f2bb2937bc7ea3c20e59e06da7938bb3fe91caf
João Neves <sevenjp@gmail.com>
2011-04-05 23:53:44 +0100

    Added missing social auth settings file.

---
+++ hojehatransportes/social_auth_settings.py
@@ -0,0 +1,9 @@
+LOGIN_ERROR_URL = '/login-error'
+
+# Twitter login details
+TWITTER_CONSUMER_KEY = ''
+TWITTER_CONSUMER_SECRET = ''
+
+# Facebook login details
+FACEBOOK_APP_ID = ''
+FACEBOOK_API_SECRET = ''
