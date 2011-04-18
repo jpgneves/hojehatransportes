5b1b14dc6fc5e41c9fb0a11d6c31f0b3bad8b380
Carlos Fonseca <carlosefonseca@gmail.com>
2011-04-18 22:36:06 +0100

    Localização PT
    Signed-off-by: Carlos Fonseca <carlosefonseca@gmail.com>

--- hojehatransportes/settings.py
+++ hojehatransportes/settings.py
@@ -12,12 +12,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '%s/blah.sqlite' % os.getcwd(), # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+		'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+		'NAME': 'hagreve', # Or path to database file if using sqlite3.
+		'USER': 'root',                      # Not used with sqlite3.
+		'PASSWORD': '',                  # Not used with sqlite3.
+		'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+		'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -32,7 +32,7 @@ TIME_ZONE = 'Europe/Lisbon'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-US'
+LANGUAGE_CODE = 'pt-PT'
 
 SITE_ID = 1
 
