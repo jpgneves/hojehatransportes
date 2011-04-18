75e950b8240a0d343681a51b5f565fc42ae5ef24
Filipe Cabecinhas <filcab+git@gmail.com>
2011-04-19 00:03:12 +0100

    Tira "empresa" dos feeds iCal

--- hojehatransportes/hat/feeds.py
+++ hojehatransportes/hat/feeds.py
@@ -80,7 +80,7 @@ class IcsFeed(Events):
         return self.item_comment(strike)
 
     def item_comment(self, strike): #TODO: Correct this for all-day events
-        return 'Greve da empresa ' + strike.company.name + '\n' + 'De ' + str(strike.start_date) + ' a ' + str(strike.end_date) + '\n' + strike.description
+        return 'Greve da ' + strike.company.name + '\n' + 'De ' + str(strike.start_date) + ' a ' + str(strike.end_date) + '\n' + strike.description
 
     def item_link(self, strike):
         return 'https://hagreve.com'
