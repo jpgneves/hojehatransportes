f6f72f56afa335e2dce478c3ff7ebb7ecbac7be2
João Neves <sevenjp@gmail.com>
2011-04-18 23:06:52 +0100

    Reverted settings to use sqlite for easier local testing.
    Changed strike description field type to textarea.

--- hojehatransportes/settings.py
+++ hojehatransportes/settings.py
@@ -12,8 +12,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-		'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-		'NAME': 'hagreve', # Or path to database file if using sqlite3.
+		'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+		'NAME': '%s/blah.sqlite' % os.getcwd(), # Or path to database file if using sqlite3.
 		'USER': 'root',                      # Not used with sqlite3.
 		'PASSWORD': '',                  # Not used with sqlite3.
 		'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
