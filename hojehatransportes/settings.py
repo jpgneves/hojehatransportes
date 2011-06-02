00cc2fa8417a8553d87d20e6b1607c87d90692a2
João Neves <sevenjp@gmail.com>
2011-06-02 17:32:01 +0100

    Added infrastructure to support redirects after login.

--- hojehatransportes/settings.py
+++ hojehatransportes/settings.py
@@ -87,6 +87,14 @@ TEMPLATE_DIRS = (
     "%s/templates" % os.getcwd()
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+)
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
