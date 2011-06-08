6aaf4a6f87c4f05696c2b541c9d5ead2b368390c
Carlos Fonseca <carlosefonseca@gmail.com>
2011-06-08 01:20:32 +0100

    working version of fabric. fixed problems with repo, settings… migrations need fixing as they're not working remotely #23
    Signed-off-by: Carlos Fonseca <carlosefonseca@gmail.com>

--- hojehatransportes/fabfile.py
+++ hojehatransportes/fabfile.py
@@ -1,26 +1,53 @@
 from fabric.api import local, settings, cd, run, env
+from fabric.colors import green
+
 
 REPO_URL = "git://github.com/jpgneves/hojehatransportes.git"
 
 env.user = "hagreve"
 
 def _deploy(path):
-    local("python manage.py schemamigration hat --auto")
-    local("git add hat/migrations/*.py")
-    local("git commit -m '[Fabric] Added new migrations'")
-    local("git push")
+    # with settings(warn_only=True):
+    #     local("python manage.py schemamigration hat --auto")
+    # local("git add hat/migrations/*.py")
+    # with settings(warn_only=True):
+    #     local("git commit -m '[Fabric] Added new migrations'")
+    # local("git push")
     with settings(warn_only=True):
-        if run("test -d %s" % path).failed:
-            run("git clone %s %s" % (REPO_URL, path))
-    with cd(path):
+
+        # REPO
+        if run("test -d %s/repo" % path).failed:
+            print(green("Cloning repo."))
+            run("git clone %s %s/repo" % (REPO_URL, path))
+
+        # Link to the relevant folder on the repo
+        if run("test -d %s/hojehatransportes" % path).failed:
+            print(green("Linking hojehatransportes to folder in repo."))
+            run("ln -s %s/repo/hojehatransportes %s/hojehatransportes" % (path, path))
+
+        # copy settings from 'root'
+        if run("test -f %s/hojehatransportes/settings.py" % path).failed:
+            print(green("Copying settings file."))
+            run("cp %s/settings.py %s/hojehatransportes" % (path, path))
+
+    # update files and restart
+    with cd("%s/hojehatransportes" % path):
+        print(green("Updating repo, migrating and restarting."))
         run("git pull")
-        run("python manage.py migrate hat")
-        run("touch %s/../tmp/restart.txt" % path)
+        with settings(warn_only=True):
+            run("python manage.py syncdb")
+            run("python manage.py migrate hat")
+        run("touch %s/tmp/restart.txt" % path)
+        print(green("All done."))
 
 def deploy_to_testing():
-    path = '/home/hagreve/test.hagreve.com/hojehatransportes'
+    path = '/home/hagreve/test.hagreve.com'
     _deploy(path)
 
 def deploy_to_production():
-    path = '/home/hagreve/hagreve.com/hojehatransportes'
+    path = '/home/hagreve/hagreve.com'
     _deploy(path)
+
+
+def howto():
+    print("To run on remote server:\n\tfab -H hagreve.com deploy_to_[testing|production]")
