a9499af0b8e427f5291a23cfe7c7a165b68a7153
Carlos Fonseca <carlosefonseca@gmail.com>
2015-01-22 21:24:06 +0000

    fixed data bug

---
+++ source-me.sh
@@ -0,0 +1,6 @@
+alias hgSource="source /Users/carlos/Code/hojehatransportes/venv/bin/activate"
+
+export DATABASE_URL="postgres://carlos@localhost:5432/carlos"
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
